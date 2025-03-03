dde58d2c727a9bfdbd467ca0e45da04f8049b91d
hamza-boody <boodyhamzo@gmail.com>
2025-03-03 21:22:34 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,34 @@
+
+// // let i=0
+
+// // do{
+// //     document.write('hello guys');i++;
+
+// // }while(i>100)
+//     let i=2
+//     do{
+    
+//         document.write(Number(i));i+=2;
+//     }while(i<=10)
+ let a=  Number (prompt("enter your num1"))
+let b=  Number (prompt("Enter your num2"))
+let o=prompt("enter your operation")
+
+    switch(o){
+
+        case"+": document.write(a+b)
+
+        break;
+
+        case"/": document.write(a/b)
+        break;
+        case"-": document.write(a-b)
+
+        case"*": document.write(a*b)
+case"%": document.write(a%b)
+
+
+default:
+    document.write("you have an error")
+
+    }
